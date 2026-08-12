e72942fdb248f5dfdbea0ce8e95ae30154c8c3e3
Mohit99Chand <chand99mohit@gmail.com>
2026-08-12 23:55:36 +0530

    Reversing the user-defined digit

---
+++ easy/02_Reverse_digit.py
@@ -0,0 +1,14 @@
+#Given an Integer n, find the reverse of its digits.
+# Input         Output          
+#  122            221
+#  200            2
+# 12345           54321
+
+"""
+There are three approaches to this problem-
+Approach1: Reversing digit by digit - we move forward by storing remainder value obtained by dividing the number with 10.
+Approach2: Using list and reverse function 
+Approach3: Using String slicing
+
+"""
+
