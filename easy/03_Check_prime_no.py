4dcffbf52c5ebf00951b75c60930d056a3af0e66
Mohit99Chand <chand99mohit@gmail.com>
2026-08-13 23:37:17 +0530

    Check for  prime no

---
+++ easy/03_Check_prime_no.py
@@ -0,0 +1,11 @@
+# Given a number n, check whether it is a prime number or not.
+# Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+
+"""
+There are three ways to solve this problem:
+Approach1:  Basic trial division
+Approach2: Square root trial division
+Approach3: Optimized square root trail division
+"""
+
