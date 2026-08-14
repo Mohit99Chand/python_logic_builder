aee7b0b33edf198a8daa95a697e32c239bb4aa07
Mohit99Chand <chand99mohit@gmail.com>
2026-08-14 23:23:31 +0530

    Checking power of a user-defined  number

---
+++ easy/04_check_power.py
@@ -0,0 +1,13 @@
+# Given two positive numbers x and y, check if y is a power of x or not.
+#  Input             Output
+#   2                   8
+#   10                  1
+#   10                  1001
+
+
+""""
+There are 3 approaches to this problem:
+Approach1: Repeated multiplication method
+Approach2: Exponential and binary search method
+Approach3: Logarithmic method
+"""
