98dcd43215de915f74914c31e091c0ec3beaa26a
Mohit99Chand <chand99mohit@gmail.com>
2026-08-17 00:00:12 +0530

    Factorial of a number

---
+++ easy/06_factorial.py
@@ -0,0 +1,7 @@
+# Given a non-negative integers n, compute the factorial of the given number. 
+# Factorial of n is defined as n * (n -1) * (n - 2) * ... * 1. For n = 0, the factorial is defined as 1.
+# Input       Output
+#   5          120
+#   4          24
+#   0          1
+#   1          1
