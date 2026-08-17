fd2a7d84f332538eee815375fd9e8fd1ad75e2fb
Mohit99Chand <chand99mohit@gmail.com>
2026-08-17 23:38:36 +0530

    Finding a pair of number whose resultant is a cube

---
+++ easy/07_pair_cube_count.py
@@ -0,0 +1,12 @@
+# Given n, count all 'a' and 'b' that satisfy the condition a^3 + b^3 = n.
+# Where (a, b) and (b, a) are considered two different pairs
+# Input                 Output          Explaination
+#   9                     2                 1^3 + 2^3 = 9 & 2^3+1^3 = 9
+#   28                    2                 1^3 + 3^3 = 28 & 3^3+1^3 = 28
+
+
+"""
+There are 2 possible approach to this problem.
+Approach1: Using nested loop
+Approach2: Using different pair
+"""
