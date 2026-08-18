b980ddc473ca6b703ae86136828b3dbb1a799981
Mohit99Chand <chand99mohit@gmail.com>
2026-08-18 22:22:05 +0530

    Finding the GCD or HCF of a given number

---
+++ easy/08_GCD_HCF.py
@@ -0,0 +1,17 @@
+# Given two positive integers a and b, the task is to find the GCD of the two numbers.
+# Note: The GCD (Greatest Common Divisor) or HCF (Highest Common Factor) of two numbers 
+# is the largest number that divides both of them. 
+
+#   Input                 Output
+#   a=20, b=28              4
+#   a=60, b=36              12
+
+""" 
+There are 5 approaches to this solution.
+Approach1: Using loop
+Approach2: Euclidean algorithm using subtraction
+Approach3: Modified Euclidean algorithm using subtraction by checking divisibility 
+Approach4: Optimized Euclidean algorithm by checking reminder
+Approach5: Using built-in function 
+
+"""
