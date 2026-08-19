ef95429eab6fcfc8e45e1b4a322b511362645a28
Mohit99Chand <chand99mohit@gmail.com>
2026-08-19 23:25:06 +0530

    LCM of a number

---
+++ easy/09_LCM.py
@@ -0,0 +1,12 @@
+# Given two positive integers a and b. Find the Least Common Multiple (LCM) of a and b.
+# Note: LCM of two numbers is the smallest number which can be divided by both numbers
+#    Input             Output
+# a=10, b=5             10
+# a=5, b=11             55
+
+"""
+There are 2 ways to solve this problem:
+Approach1: Using conditional loop
+Approach2: Using GCD LCM Formula
+
+"""
