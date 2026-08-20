6066e2dd9c77cec22f024c90bc91f4aa58cb921f
Mohit99Chand <chand99mohit@gmail.com>
2026-08-20 23:10:20 +0530

    Finding weather the given number is a perfect no

---
+++ easy/10_perfect_number.py
@@ -0,0 +1,13 @@
+# Find whether a given positive integer n is perfect or not.
+# A number is a perfect number if it is equal to the sum of its proper divisors, that is,
+# the sum of its positive divisors excluding the number itself.
+# Input             Output
+#  15                False
+#  6                 True
+
+
+"""
+There are two approaches to this problem:
+Approach1: Divisor sum method
+Approach2: Optimised divisior sum method
+"""
