abc0617112195c8948a07de7fc3cf519ecad5568
Mohit99Chand <chand99mohit@gmail.com>
2026-08-21 23:03:44 +0530

    Find the sum of two fraction

---
+++ easy/11_add_two_fraction.py
@@ -0,0 +1,10 @@
+# Given two integer arrays a[] and b[] containing two integers each representing the numerator and denominator of a fraction respectively. 
+# The task is to find the sum of the two fractions and return the numerator and denominator of the result.
+# Input                       Output            Explaination
+# a=[1,2], b=[3,2]             [2,1]              1/2+3/2=2/1
+# a=[1,3], b=[3,9]             [2,3]              1/3+3/9=2/3
+# a=[1,5], b=[3,15]            [2,5]              1/5+3/15=2/5
+
+"""
+The is only one approach and the approach is 
+"""
