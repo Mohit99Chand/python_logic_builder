4993fdf89d2cb341c25c0ce53b6b56ff652421a7
Mohit99Chand <chand99mohit@gmail.com>
2026-08-22 23:52:37 +0530

    Finding the Day of a week on basis of given date

---
+++ easy/12_day_of_week.py
@@ -0,0 +1,12 @@
+# Given an array date[] = [d, m, y], where d denotes the day, m denotes the month, and y denotes the year,
+# Write a program that calculates the day of the week for any particular date in the past or future.
+#   Input(d,m,y)                   Output
+#    30,8,2010                      1
+#    15,6,1995                      4
+#    29,2,2016                      1
+
+"""
+There are 2 approaches to this problem:
+Approach 1: Day count calculation
+Approach 2: Zeller's Congruence Formula
+"""
