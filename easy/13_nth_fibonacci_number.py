ea5113d02f23ab7f3a90edbf7b621c15740c3124
Mohit99Chand <chand99mohit@gmail.com>
2026-08-23 23:40:04 +0530

    Finding the number on the nth term

---
+++ easy/13_nth_fibonacci_number.py
@@ -0,0 +1,18 @@
+# Given a positive integer n, find the nth Fibonacci number.
+# The Fibonacci series is a sequence where a term is the sum of previous two terms. 
+# The first two terms of the Fibonacci sequence are 0 followed by 1. 
+# The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21.
+
+# Input                 Output
+#  n=2                    1
+#  n=5                    5
+
+"""
+There are 3 approach to this problem:
+Approach 1: Using recursion
+Approach 2: Memoization 
+Approach 3: Bottom-up
+Approach 4: Bottom-up space optimized 
+Approach 5: Using matrix exponentiation
+Approach 6: Using golden ratio
+"""
