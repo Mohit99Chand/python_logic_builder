92324a36734810022aa8c22ea42f9b60a86ac2e7
Mohit99Chand <chand99mohit@gmail.com>
2026-08-24 23:57:39 +0530

    Converting the given decimal value into binary

---
+++ easy/14_decimal_to_binary.py
@@ -0,0 +1,13 @@
+# Given a non negative number n, the task is to convert the given number into an equivalent binary representation.
+#   Input               Output
+#    12                  1100
+#    33                  10001
+
+"""
+There are 4 approaches to this problem-
+Approach 1: Division by 2
+Approach 2: Using head recursion
+Approach 3: Using Bit-wise operator
+Approach 4: Using built-in method
+
+"""
