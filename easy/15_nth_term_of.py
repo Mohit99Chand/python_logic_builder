9dd7b8260569391ce13b606bb81cc08ba38b2488
Mohit99Chand <chand99mohit@gmail.com>
2026-08-25 23:44:11 +0530

    Finding the nth term of 1,3,6,10,15,21...

---
+++ easy/15_nth_term_of.py
@@ -0,0 +1,11 @@
+
+# Given a number n, find the n-th term in the series 1, 3, 6, 10, 15, 21...
+# Input             Output
+#   3                 6
+#   4                 10
+
+"""
+There are 2 approaches to solve this problem:
+Approach 1: Using Loop
+Approach 2: Using formula
+"""
