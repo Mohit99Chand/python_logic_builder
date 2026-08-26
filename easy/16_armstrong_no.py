75341d62e930cf82b0e78d4d01d4e8d4ac29e7b6
Mohit99Chand <chand99mohit@gmail.com>
2026-08-26 23:59:10 +0530

    Finding the armstrong of a given number

---
+++ easy/16_armstrong_no.py
@@ -0,0 +1,15 @@
+# Given a number x, determine whether the given number is Armstrong's number or not. 
+# A positive integer of n digits is called an Armstrong number of order n (order is the number of digits) if
+# abcd... = pow(a,n) + pow(b,n) + pow(c,n) + pow(d,n) + ....
+# Here a, b, c and d are digits of input number abcd.....
+#   Input                   Output
+#    153                     True
+#    9474                    True
+#    123                     False
+
+
+"""
+There are 2 approaches to solve this problem:
+Approach 1: Naive
+Approach 2: Using numeric strings
+"""
