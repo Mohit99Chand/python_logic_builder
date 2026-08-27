c51468aacd95265f8f741e6a1b0ca2163070dbf0
Mohit99Chand <chand99mohit@gmail.com>
2026-08-28 00:12:09 +0530

    Finding the digital root of a number

---
+++ easy/18_digital_root.py
@@ -0,0 +1,12 @@
+# Given a number n, find the digital root of n. 
+# Digital Root of a number is the recursive sum of its digits until we get a single digit number.
+#   Input           Output
+#     1                1
+#    99999             9
+
+
+"""
+There are 2 approach to this problem:
+Approach 1: Repetitively adding digits
+Aprroach 2: Using mathematical formula
+"""
