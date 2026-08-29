ad195505e889601a409e11eae69f5c09db085115
Mohit99Chand <chand99mohit@gmail.com>
2026-08-29 13:59:01 +0530

    Finding the square root of integer data type

---
+++ hard/01_square_root.py
@@ -0,0 +1,12 @@
+# Given a positive integer n, find its square root. If n is not a perfect square, then return floor of √n.
+#   Input       Output
+#     4           2
+#     11          3
+
+"""
+There are 4 approaches to this problem:
+Approach 1: Using loop
+Approach 2: Using Binary search
+Approach 3: Using Built-in Function
+Approach 4: Using formula used by Pocket calculators
+"""
