67d33610276b01c019f3a8decf7ed3819f043f9a
Mohit99Chand <chand99mohit@gmail.com>
2026-08-31 19:17:42 +0530

    Finding exactly 3 divisor of a given number

---
+++ hard/02_divisibility_by_3.py
@@ -0,0 +1,12 @@
+# You are given an integer array queries[], where each element represents a query value n. 
+# For every n in the array, determine how many numbers less than or equal to n have exactly 3 divisors.
+#   Input               Output
+#    queries[]=[6]         [1]
+#    query[]=[3,10]        [0,2]
+
+"""
+There are 3 approaches to this problem:
+Approach 1: Divisor count check
+Approach 2: Prime Square Property
+Approach 3: Sieve With Prefix count
+""" 
