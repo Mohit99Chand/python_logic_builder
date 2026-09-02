8da3998177f7c07820038307798f603f9e4cf5cc
Mohit99Chand <chand99mohit@gmail.com>
2026-09-02 23:58:34 +0530

    checking if a number can be  divisible by 13

---
+++ hard/04_divisibility_by_13.py
@@ -0,0 +1,11 @@
+# Given a number s represented as a string, determine whether the integer it represents is divisible by 13 or not.
+#   Input               Output
+#    2911285              True
+#    27                   False
+
+"""
+There are 3 approaches to this problem:
+Approach 1: Modulo division 
+Approach 2: Alterantive sum of 3 digit blocks
+Approach 3: String-based modulo
+"""  
