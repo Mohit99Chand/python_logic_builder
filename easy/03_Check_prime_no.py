8d1e4866015260d2c59d08ae13e91255085e1350
mohit99chand <chand99mohit@gmail.com>
2026-09-20 01:54:11 +0530

    code + logic updated

--- easy/03_Check_prime_no.py
+++ easy/03_Check_prime_no.py
@@ -9,3 +9,16 @@ Approach2: Square root trial division
 Approach3: Optimized square root trail division
 """
 
+#Approach 1: Basic trial division
+def check_prime(n):
+    if (n==2 or n==3 or n==5):
+        return f"{n} is a prime no"
+    elif ((n <= 0) or (n%2 == 0) or (n%3 == 0) or (n%5 == 0)):
+        return f"{n} is not a prime no"
+    else:
+        return f"{n} is a prime no"
+
+if __name__ == "__main__":
+    num = int(input("Enter the value: "))
+    print(check_prime(num))
+
